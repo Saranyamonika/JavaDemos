61e10a1c868d098afe3f5114d214b7e2d914fdf0
Saranyamonika <40536600+Saranyamonika@users.noreply.github.com>
2018-08-25 22:19:18 -1200

    collections demo

---
+++ TestCollection.java
@@ -0,0 +1,18 @@
+package javademo;
+
+import java.util.ArrayList;
+
+public class TestCollection {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> a=new ArrayList<Integer>();
+		a.add(10);
+		a.add(20);
+		a.add(30);
+		a.add(5);
+		a.add(15);
+		a.add(30);
+		System.out.println(a);
+	}
+
+}
